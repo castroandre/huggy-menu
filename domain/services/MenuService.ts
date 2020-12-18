ed15c71abb54615cda7d857c0228b604437474ee
Andre Castro <andre.v@outlook.com>
2020-12-17 21:01:55 -0300

    🐛 Fix erro in init _route

--- domain/services/MenuService.ts
+++ domain/services/MenuService.ts
@@ -5,7 +5,6 @@ export class MenuService {
   constructor(resource: any, route: string = '') {
     this._resource = resource;
     this._route = 'https://script.google.com/macros/s/AKfycbz48y_tLVr-Yk_N2D-ey25wtIVNPwD2T_5DcNQ0hrB9sKSJ8gk/exec';
-    if (route) this._route = this._route + '/' + route;
   }
 
   get() {
